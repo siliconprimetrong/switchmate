8347058ffa6c2d37018c767ff9a90f367d136aaf
siliconprimetrong <trong@siliconprime.us>
2014-08-11 14:43:25 +0700

    commit
    commit

---
+++ Switchmate/README.md
@@ -0,0 +1,11 @@
+# Switchmate Website
+
+
+New website for Switchmate by Adam Greenough of Amazorize.
+
+Required from client:
+- Animation frames/source files
+- Any further copy
+- Google Analytics permissions (adam@amazorize.com) and goal specifics
+
+## Blog and contact form to be completed serverside with PHP support
---
+++ Switchmate/js/homepage.js
@@ -0,0 +1,42 @@
+﻿$(function() {
+    $('.play-video').click(function () {
+        $('.video-youtube').css('display', 'block');
+        $(this).hide();
+        $('.video-youtube').html("<iframe width='420' height='315' src='http://www.youtube.com/embed/twyOLniei9c?autoplay=1' frameborder='0' allowfullscreen></iframe>");
+    });
+
+    $('.collapse').click(function () {
+        if ($('.top-bar-section .right').hasClass('hide-collapse')) {
+            $('.top-bar-section .right').removeClass('hide-collapse');
+        } else {
+            $('.top-bar-section .right').addClass('hide-collapse');
+        }
+    });
+    $('.notifiednav').click(function() {
+        $('.reveal-modal').css('top', '-50px');
+    });
+    $('#mc-embedded-subscribe-form').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            email: "Please enter a valid email address"
+        }
+    });
+    $('#index-form-subcribe').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            email: "Please enter a valid email address"
+        }
+    });
+})
+
+
